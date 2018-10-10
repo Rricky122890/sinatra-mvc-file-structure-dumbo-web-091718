1903ac2bb2e8d9397145a5a630c4ddf926746612
Carolyn Rosemary Winifred Cole <carolynrosemarywinifredcole@Carolyns-MacBook-Air.local>
2018-10-10 11:30:19 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,22 @@
 class ApplicationController < Sinatra::Base
 
+  # class Dog
+  # attr_accessor :name, :breed, :age
+  #
+  # def initialize(name, breed, age)
+  #   @name = name
+  #   @breed = breed
+  #   @age = age
+  # end
+  #
+  # end
+
   configure do
   	set :views, "app/views"
   	set :public_dir, "public"
   end
+
+
 
   get "/" do
   	erb :index
